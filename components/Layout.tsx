1e29348f956b14f8813f0b947e72bd4360e4d2e5
pedrokstro <pedrokstro@users.noreply.github.com>
2026-01-06 21:26:39 -0300

    style: remove mobile top navbar to increase screen space

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Sidebar } from './Sidebar';
-import { Navbar } from './Navbar';
 import { MobileNav } from './MobileNav';
 
 export const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
@@ -8,7 +7,6 @@ export const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) =>
     <div className="flex h-screen bg-background-light dark:bg-background-dark overflow-hidden transition-colors duration-300">
       <Sidebar />
       <div className="flex-1 flex flex-col min-w-0 overflow-hidden relative">
-        <Navbar />
         <main className="flex-1 overflow-y-auto p-4 md:p-8 pb-24 md:pb-8">
           {children}
         </main>
